7bce2f3ab9676fae7b50daf7c7989bd7049f3919
Martin <martin.piegay@zenika.com>
2016-06-09 12:52:22 +0200

    add func decodeHookSlice + refactor generateMapstructure

--- kv.go
+++ kv.go
@@ -2,35 +2,82 @@ package staert
 
 import (
 	"fmt"
+	"github.com/docker/libkv/store"
+	"reflect"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-func generateMapstructure(input map[string]string, prefix string) (map[string]interface{}, error) {
+func generateMapstructure(pairs []*store.KVPair, prefix string) (map[string]interface{}, error) {
 	raw := make(map[string]interface{})
-	for k, v := range input {
+	for _, p := range pairs {
 		// Trim the prefix off our key first
-		key := strings.TrimPrefix(k, prefix)
-
-		// Determine what map we're writing the value to. We split by '/'
-		// to determine any sub-maps that need to be created.
-		m := raw
-		children := strings.Split(key, "/")
-		if len(children) > 0 {
-			key = children[len(children)-1]
-			children = children[:len(children)-1]
-			for _, child := range children {
-				if m[child] == nil {
-					m[child] = make(map[string]interface{})
-				}
-				subm, ok := m[child].(map[string]interface{})
-				if !ok {
-					return nil, fmt.Errorf("child is both a data item and dir: %s", child)
-				}
-				m = subm
-			}
-
+		key := strings.TrimPrefix(p.Key, prefix)
+		raw, err := processKV(key, string(p.Value), raw)
+		if err != nil {
+			return raw, err
 		}
-		m[key] = string(v)
+
 	}
 	return raw, nil
+}
+
+func processKV(key string, v string, raw map[string]interface{}) (map[string]interface{}, error) {
+	// Determine what map we're writing the value to. We split by '/'
+	// to determine any sub-maps that need to be created.
+	m := raw
+	children := strings.Split(key, "/")
+	if len(children) > 0 {
+		key = children[len(children)-1]
+		children = children[:len(children)-1]
+		for _, child := range children {
+			if m[child] == nil {
+				m[child] = make(map[string]interface{})
+			}
+			subm, ok := m[child].(map[string]interface{})
+			if !ok {
+				return nil, fmt.Errorf("child is both a data item and dir: %s", child)
+			}
+			m = subm
+		}
+
+	}
+	m[key] = string(v)
+
+	return raw, nil
+}
+
+func decodeHookSlice(fromType reflect.Type, toType reflect.Type, data interface{}) (interface{}, error) {
+	if toType.Kind() == reflect.Slice {
+		if fromType.Kind() == reflect.Map {
+			// Type assertion
+			dataMap, ok := data.(map[string]interface{})
+			if !ok {
+				return data, fmt.Errorf("input data is not a map : %#v", data)
+			}
+			// Sorting map
+			indexes := make([]int, len(dataMap))
+			i := 0
+			for k := range dataMap {
+				ind, err := strconv.Atoi(k)
+				if err != nil {
+					return dataMap, err
+				}
+				indexes[i] = ind
+				i++
+			}
+			sort.Ints(indexes)
+			// Building slice
+			dataOutput := make([]interface{}, i)
+			i = 0
+			for _, k := range indexes {
+				dataOutput[i] = dataMap[strconv.Itoa(k)]
+				i++
+			}
+
+			return dataOutput, nil
+		}
+	}
+	return data, nil
 }
--- kv_test.go
+++ kv_test.go
@@ -1,18 +1,25 @@
 package staert
 
 import (
+	"github.com/docker/libkv/store"
 	"reflect"
 	"testing"
 )
 
 func TestGenerateMapstructureBasic(t *testing.T) {
-	input := map[string]string{
-		"test/addr":       "foo",
-		"test/child/data": "bar",
+	moke := []*store.KVPair{
+		&store.KVPair{
+			Key:   "test/addr",
+			Value: []byte("foo"),
+		},
+		&store.KVPair{
+			Key:   "test/child/data",
+			Value: []byte("bar"),
+		},
 	}
 	prefix := "test/"
 
-	output, err := generateMapstructure(input, prefix)
+	output, err := generateMapstructure(moke, prefix)
 	if err != nil {
 		t.Fatalf("Error :%s", err)
 	}
@@ -29,14 +36,23 @@ func TestGenerateMapstructureBasic(t *testing.T) {
 }
 
 func TestGenerateMapstructureTrivialMap(t *testing.T) {
-	input := map[string]string{
-		"test/vfoo":       "foo",
-		"test/vother/foo": "foo",
-		"test/vother/bar": "bar",
+	moke := []*store.KVPair{
+		&store.KVPair{
+			Key:   "test/vfoo",
+			Value: []byte("foo"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/foo",
+			Value: []byte("foo"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/bar",
+			Value: []byte("bar"),
+		},
 	}
 	prefix := "test/"
 
-	output, err := generateMapstructure(input, prefix)
+	output, err := generateMapstructure(moke, prefix)
 	if err != nil {
 		t.Fatalf("Error :%s", err)
 	}
@@ -54,15 +70,27 @@ func TestGenerateMapstructureTrivialMap(t *testing.T) {
 }
 
 func TestGenerateMapstructureTrivialSlice(t *testing.T) {
-	input := map[string]string{
-		"test/vfoo":     "foo",
-		"test/vother/0": "foo",
-		"test/vother/1": "bar",
-		"test/vother/2": "bar",
+	moke := []*store.KVPair{
+		&store.KVPair{
+			Key:   "test/vfoo",
+			Value: []byte("foo"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/0",
+			Value: []byte("foo"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/1",
+			Value: []byte("bar1"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/2",
+			Value: []byte("bar2"),
+		},
 	}
 	prefix := "test/"
 
-	output, err := generateMapstructure(input, prefix)
+	output, err := generateMapstructure(moke, prefix)
 	if err != nil {
 		t.Fatalf("Error :%s", err)
 	}
@@ -71,8 +99,8 @@ func TestGenerateMapstructureTrivialSlice(t *testing.T) {
 		"vfoo": "foo",
 		"vother": map[string]interface{}{
 			"0": "foo",
-			"1": "bar",
-			"2": "bar",
+			"1": "bar1",
+			"2": "bar2",
 		},
 	}
 	if !reflect.DeepEqual(check, output) {
@@ -81,16 +109,31 @@ func TestGenerateMapstructureTrivialSlice(t *testing.T) {
 }
 
 func TestGenerateMapstructureNotTrivialSlice(t *testing.T) {
-	input := map[string]string{
-		"test/vfoo":          "foo",
-		"test/vother/0/foo1": "bar",
-		"test/vother/0/foo2": "bar",
-		"test/vother/1/bar1": "foo",
-		"test/vother/1/bar2": "foo",
+	moke := []*store.KVPair{
+		&store.KVPair{
+			Key:   "test/vfoo",
+			Value: []byte("foo"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/0/foo1",
+			Value: []byte("bar"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/0/foo2",
+			Value: []byte("bar"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/1/bar1",
+			Value: []byte("foo"),
+		},
+		&store.KVPair{
+			Key:   "test/vother/1/bar2",
+			Value: []byte("foo"),
+		},
 	}
 	prefix := "test/"
 
-	output, err := generateMapstructure(input, prefix)
+	output, err := generateMapstructure(moke, prefix)
 	if err != nil {
 		t.Fatalf("Error :%s", err)
 	}
@@ -111,4 +154,45 @@ func TestGenerateMapstructureNotTrivialSlice(t *testing.T) {
 	if !reflect.DeepEqual(check, output) {
 		t.Fatalf("Expected %#v\nGot %#v", check, output)
 	}
+}
+
+func TestDecodeHookSlice(t *testing.T) {
+	type BasicStruct struct {
+		Bar1 string
+		Bar2 string
+	}
+	type SliceStruct []BasicStruct
+	type Test struct {
+		Vfoo   string
+		Vother SliceStruct
+	}
+	data := map[string]interface{}{
+		"10": map[string]interface{}{
+			"bar1": "bar1",
+			"bar2": "bar2",
+		},
+		"2": map[string]interface{}{
+			"bar1": "foo1",
+			"bar2": "foo2",
+		},
+	}
+	output, err := decodeHookSlice(reflect.TypeOf(data), reflect.TypeOf([]string{}), data)
+	if err != nil {
+		t.Fatalf("Error : %s", err)
+	}
+
+	check := []interface{}{
+		map[string]interface{}{
+			"bar1": "foo1",
+			"bar2": "foo2",
+		},
+		map[string]interface{}{
+			"bar1": "bar1",
+			"bar2": "bar2",
+		},
+	}
+	if !reflect.DeepEqual(check, output) {
+		t.Fatalf("Expected %#v\nGot %#v", check, output)
+	}
+
 }
